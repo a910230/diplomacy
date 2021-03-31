480185100a5676ab980e475c81a7a27a9031033a
a910230 <a910230@gmail.com>
2021-03-31 16:20:16 -0500

    test

--- server.go
+++ server.go
@@ -15,7 +15,7 @@ func main() {
 	mux.HandleFunc("/solver", solverHandler)
 	http.Handle("/game", http.FileServer(http.Dir("/game")))
 	// mux.HandleFunc("/game", gameHandler)
-	mux.HandleFunc("/", indexHandler)
+	// mux.HandleFunc("/", indexHandler)
 
 	// Start a web server.
 	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
