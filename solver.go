4c38315c17205d51dec6e399812b2bbf39f83b46
a910230 <a910230@gmail.com>
2021-03-31 14:25:19 -0500

    change path

--- solver.go
+++ solver.go
@@ -59,9 +59,9 @@ func solverHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func gameHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "game/game.html")
+	http.ServeFile(w, r, "~/diplomacy-server/game/game.html")
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+	http.ServeFile(w, r, "~/diplomacy-server/index.html")
 }
