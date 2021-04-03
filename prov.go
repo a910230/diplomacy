911f2a66d695b08f9c1cac1ac6706d643401f68f
a910230 <a910230@gmail.com>
2021-04-02 23:52:44 -0500

    prov

---
+++ prov.go
@@ -0,0 +1,20 @@
+package main
+
+var (
+	players = map[string]string{
+		"aus": "Alice",
+		"eng": "Bob",
+		"fra": "Charlie",
+		"ger": "David",
+		"ita": "Eve",
+		"rus": "Frank",
+		"tur": "George"}
+	prov = []string{"boh", "bud", "gal", "tri", "tyr", "vie", "cly", "edi", "lvp",
+		"lon", "wal", "yor", "bre", "bur", "gas", "mar", "par", "pic", "ber", "kie",
+		"mun", "pru", "ruh", "sil", "apu", "nap", "pie", "rom", "tus", "ven", "fin",
+		"lvn", "mos", "sev", "stpn", "stps", "ukr", "war", "ank", "arm", "con", "smy",
+		"syr", "alb", "bel", "buln", "buls", "den", "gre", "hol", "nwy", "naf", "por",
+		"rum", "ser", "span", "spas", "swe", "tun", "adr", "aeg", "bal", "bar", "bla",
+		"eas", "eng", "bot", "gol", "hel", "ion", "iri", "mid", "nat", "nth", "nrg",
+		"ska", "tyn", "wes"}
+)
